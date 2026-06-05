acae9f194222a434f485f528d692cdc96b72f83c
Junaid-Devops <107377867+Junaid-Devops@users.noreply.github.com>
2026-06-05 09:30:52 -0400

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,12 @@
 terraform {
-  # Leave this as >= 0.15.0 since your pipeline is running an old version
-  required_version = ">= 0.15.0" 
+  # Enforce the new version matching your SSM update
+  required_version = ">= 1.5.7"   
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # This forces Terraform to use an older v4.x release, bypassing the key error
-      version = "~> 4.67.0" 
+      # You can now safely use v5.x or v6.x versions!
+      version = "~> 5.0" 
     }
   }
 }
