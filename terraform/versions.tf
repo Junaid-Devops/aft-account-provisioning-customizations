609eebbe95bf8675e2ce911544311780c98d6f25
Junaid-Devops <107377867+Junaid-Devops@users.noreply.github.com>
2026-06-05 09:25:27 -0400

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,12 @@
 terraform {
-  #required_version = ">= 0.15.0"
-  required_version = ">= 1.5.7"
+  # Leave this as >= 0.15.0 since your pipeline is running an old version
+  required_version = ">= 0.15.0" 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-     version = "~> 6.38"
+      # This forces Terraform to use an older v4.x release, bypassing the key error
+      version = "~> 4.67.0" 
     }
   }
 }
